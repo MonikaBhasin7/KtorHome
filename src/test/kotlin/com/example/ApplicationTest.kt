ce7ba49459e945c0bd948aecc02b5f7f2cf2a43f
Monika_Bhasin <monika.bhasin@myparkplus.com>
2021-12-11 19:02:15 +0530

    Initial commit

---
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,17 @@
+package com.example
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import kotlin.test.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+
+    }
+}
