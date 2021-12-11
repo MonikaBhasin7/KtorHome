2faaab79fb9689a1b85c3a8a6d918fd9c4f09796
Monika_Bhasin <monika.bhasin@myparkplus.com>
2021-12-11 20:01:50 +0530

    Routing

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,9 +1,20 @@
 package com.example
 
 import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
 
+    routing {
+        route("/") {
+            get {
+                call.respond("He is there.")
+            }
+        }
+    }
 }
